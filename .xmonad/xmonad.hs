e0fc1d606a725c0f66b86a3405826f0263e1e7ba
jiasen <jiasen@capeanalytics.com>
2018-10-13 22:14:47 +0200

    replace xmobar with polybar

--- .xmonad/xmonad.hs
+++ .xmonad/xmonad.hs
@@ -3,25 +3,30 @@ import XMonad.Config.Desktop
 import XMonad.Hooks.DynamicLog
 import XMonad.Util.Run(spawnPipe)
 import XMonad.Actions.SpawnOn(spawnHere)
-import XMonad.Actions.Volume
+--import XMonad.Actions.Volume
 import XMonad.Util.EZConfig(additionalKeys)
 import XMonad.Hooks.Script(execScriptHook)
 import qualified Graphics.X11.ExtraTypes.XF86 as XF86
 import System.IO
 import Control.Monad
+import qualified XMonad.DBus as D
 
 main = do
-    xmproc <- spawnPipe "/home/jiasen/.cabal/bin/xmobar"
+    -- xmproc <- spawnPipe "/home/jiasen/.cabal/bin/xmobar"
+    dbus <- D.connect
+    D.requestAccess dbus
     spawn "ibus-daemon"
+    spawn "polybar laptop"
     xmonad $ desktopConfig
       { terminal = "/usr/bin/gnome-terminal"
       , borderWidth = 3
       , modMask = mod4Mask
-      , logHook = dynamicLogWithPP xmobarPP 
-                    { ppOutput = hPutStrLn xmproc
-                    , ppTitle = xmobarColor "green" "" . shorten 100
-                    , ppSep = "    "
-                    }
+      --, logHook = dynamicLogWithPP xmobarPP 
+      --              { ppOutput = hPutStrLn xmproc
+      --              , ppTitle = xmobarColor "green" "" . shorten 100
+      --              , ppSep = "    "
+      --              }
+      , logHook = dynamicLogWithPP (myLogHook dbus) 
       , startupHook = myStartupHook
       } `additionalKeys` 
       [ ((0, xK_Print), spawn "deepin-screenshot")
@@ -32,11 +37,14 @@ main = do
       -- type the password in the blank screen to login again.
       , ((mod4Mask .|. controlMask, xK_l), spawn "slock")
       -- volume adjustment (doesn't work)
-      , ((0, XF86.xF86XK_AudioLowerVolume), void $ lowerVolume 4)
-      , ((0, XF86.xF86XK_AudioRaiseVolume), void $ raiseVolume 4)
+      --, ((0, XF86.xF86XK_AudioLowerVolume), void $ lowerVolume 4)
+      --, ((0, XF86.xF86XK_AudioRaiseVolume), void $ raiseVolume 4)
       ]
 
 -- For some reason, the xmonad starts up assuming an external monitor,
 -- and leaves the internal one (eDP-1-1, if nvidia enabled) off.
 -- I call xrandr to enable it.
 myStartupHook = spawn "xrandr --output eDP-1-1 --auto" 
+
+myLogHook dbus = def { ppOutput = D.send dbus }
+
